256562c599826f8f0f2d0ec84e75c8588ecbae31
efsanees <115102300+efsanees@users.noreply.github.com>
2023-05-19 17:42:25 +0300

    Update gonder.php

--- gonder.php
+++ gonder.php
@@ -1,18 +1,13 @@
 <?php
-// Kullanıcı adı ve şifreyi al
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Kullanıcı adından öğrenci numarasını çıkart
 $studentNumber = substr($email, 0, -14);
 
-// Şifre kontrolü
 if ($password === $studentNumber) {
-    // Başarılı giriş durumunda hoş geldin mesajını göster
-    echo "Hoşgeldiniz \"$studentNumber\". Giriş işlemi başarılı.";
+    echo "Hoşgeldiniz \"$studentNumber\". GİRİŞ İŞLEMİ BAŞARILI.";
 } else {
-    // Başarısız giriş durumunda kullanıcıyı login sayfasına yönlendir
-    header("Location: login.html");
+    header("Location: giris.html");
     exit();
 }
 ?>
